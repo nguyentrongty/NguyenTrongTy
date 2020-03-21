3975cee033139a250823f851ddcb7e32a042f61a
nguyentrongty <62439357+nguyentrongty@users.noreply.github.com>
2020-03-21 16:08:04 +0700

    update homework
    nộp bài lession1

---
+++ bai_tap/src/lession4/Account.java
@@ -0,0 +1,39 @@
+package lession4;
+
+public class Account {
+     private int accountNumber;
+     private double balance;
+
+     public Account(int accountNumber, double balance) {
+         this.accountNumber = accountNumber;
+         this.balance = balance;
+     }
+     public Account(int accountNumber) {
+         this.accountNumber = accountNumber;
+         this.balance = 0.0;
+     }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+    public double getBalance() {
+         return this.balance;
+    }
+    public void setBalance(double balance){
+         this.balance = balance;
+    }
+    public void credit(double amount) {
+         balance += amount;
+    }
+    public void debit(double amount) {
+         if (balance < amount) {
+             System.out.println("amount withdrawn exceeds the curren balance!");
+         }else {
+             balance -= amount;
+         }
+    }
+    public String toString() {
+         return String.format("A/C no :%d, Balance=%.2f", accountNumber, balance);
+    }
+
+}
